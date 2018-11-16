6c6698060e245be1cf1d3241458f91f427dce063
Leonel Oliveira -  OnResearch Inc <loliveira@hq.onresearch.com>
2018-11-16 14:20:33 -0500

    updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,22 +8,38 @@ const Search = (props) => {
   return(
     <div className={styles['search-box']}>
 	    <div className={styles['list']}>
-	    	<div className={styles['logo-container']}>
+	    	<div className={`${styles['logo-container']} ${styles['columns']}`}>
 	    		<img src={logo} className={styles['logo']}/>
 	    	</div>
-	    	<ul className={styles['desktop-menu']}>
-					<li><a href="">Discover</a></li>
-					<li><a href="">Create</a></li>
-	    	</ul>
-	    	<input id="myInput"  type="text" className={styles['search-item']} placeholder="Search" onChange={props.searchFunc} /><div className={styles['search-icon']}>&#128269;</div>
-				<a className={`${styles['btn-login']} btn`}>Go Premium</a>
-				<a className={styles['btn-mobile']} id="myBtn">☰</a>
-			</div>
+	    	<div className={styles['columns']}>
+					<ul className={styles['desktop-menu']}>
+						<li><a href="">Discover</a></li>
+						<li><a href="">Create</a></li>
+		    	</ul>
+	    	</div>
+	    	<div className={styles['columns']}>
+						<input id="myInput"  type="text" className={styles['search-item']} placeholder="Search" onChange={props.searchFunc} /><div className={styles['search-icon']}>&#128269;</div>
+				
+	    	</div>
+
+				<div className={styles['columns']}>
+						<div className={`${styles['btn-login']} btn`}>
+						<a>Go Premium</a>
+						</div>
+						
+
+
+						<a className={styles['btn-mobile']} id="myBtn">☰</a>
+				</div>
+				
+			
 			<div className={styles['mobile']}>
 					<ul>
 						<li><a href="">Discover</a></li>
 						<li><a href="">Create</a></li>
 	    	</ul>
+			</div>
+
 			</div>
     </div>
   )
